465f2d424ce90ec7b524b6b9029b1e5faf367d71
Fabricio Céspedes <79654477+FabricioCes@users.noreply.github.com>
2023-03-12 18:25:20 -0700

    Update Firebase.js

--- Connections/Firebase.js
+++ Connections/Firebase.js
@@ -7,13 +7,14 @@ import {getStorage} from "firebase/storage"
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+//User your own credentials.
 const firebaseConfig = {
-  apiKey: "AIzaSyCu6A7z9ZSzb9kBJ05YYwntdBJdH-DTG_k",
-  authDomain: "fb-crud-react-aff5c.firebaseapp.com",
-  projectId: "fb-crud-react-aff5c",
-  storageBucket: "fb-crud-react-aff5c.appspot.com",
-  messagingSenderId: "441919959467",
-  appId: "1:441919959467:web:1bc4deada4bbb15b42e2ee"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
